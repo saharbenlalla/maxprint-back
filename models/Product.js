5bbf95ff95f4618c7cc4ecaa16e221d744550cc6
saharbenlalla <saharbenlalla212@gmail.com>
2026-06-18 11:39:27 +0100

    Fix price

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,7 @@ const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     description: { type: String },
-    price: { type: Number, required: true },
+    price: { type: Number },
     category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
     images: [String], // on peut stocker URLs
     units: { type: String }, // ex: "pièce", "kg", etc.
